eebf714bb468d57163f8c5212ebab9bc6907673f
MaciejMalina <maciejmalina19@gmail.com>
2025-03-30 18:32:50 +0200

    refactor: adding report table to database

---
+++ backend/symfony/src/Entity/Report.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ReportRepository;
+
+#[ORM\Entity(repositoryClass: ReportRepository::class)]
+class Report
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string')]
+    private string $title;
+
+    #[ORM\Column(type: 'text')]
+    private string $content;
+
+    #[ORM\Column(type: 'string')]
+    private string $status = 'open';
+
+    #[ORM\Column(type: 'datetime')]
+    private \DateTime $createdAt;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    // Gettery i settery...
+}
